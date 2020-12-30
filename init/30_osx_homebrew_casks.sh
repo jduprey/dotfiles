25a2f25bb87ac9dd502ade44a9100e4254c49890
John Duprey <john.duprey@gmail.com>
2020-12-29 20:16:59 -0500

    Update 30_osx_homebrew_casks.sh
    Mac homebrew cask install command has changed.

--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -91,7 +91,7 @@ casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
 if (( ${#casks[@]} > 0 )); then
   e_header "Installing Homebrew casks: ${casks[*]}"
   for cask in "${casks[@]}"; do
-    brew cask install $cask
+    brew install --cask $cask
   done
 fi
 
